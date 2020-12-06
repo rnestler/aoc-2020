060d5633cada08d90774386c331e7bb320a84cb0
Raphael Nestler <raphael.nestler@gmail.com>
2020-12-06 18:01:43 +0100

    Add boiler plate for next day

---
+++ aoc-2020-7/src/main.rs
@@ -0,0 +1,15 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = File::open("input.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
