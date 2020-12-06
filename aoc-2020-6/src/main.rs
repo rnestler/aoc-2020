e8ea8dbee27f2b3744eedd3cf51f2e9a3d170460
Raphael Nestler <raphael.nestler@gmail.com>
2020-12-06 09:49:44 +0100

    Day 6 part 1

--- aoc-2020-6/src/main.rs
+++ aoc-2020-6/src/main.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -5,6 +6,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut file = File::open("input.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
+
+    let groups = contents.split("\n\n");
+
+    let part1: usize = groups
+        .map(|group| {
+            let mut group: Vec<u8> = group.as_bytes().into();
+            group.sort();
+            group
+                .iter()
+                .filter(|v| **v != b'\n')
+                .map(|v| u32::from(*v))
+                .unique()
+                .count()
+        })
+        .sum();
+    println!("Part 1: {}", part1);
 
     Ok(())
 }
