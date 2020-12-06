4de365ad39d1c7de6995186153db1b8f22c5f45c
Raphael Nestler <raphael.nestler@gmail.com>
2020-12-06 11:45:04 +0100

    Day 6 part 2

--- aoc-2020-6/src/main.rs
+++ aoc-2020-6/src/main.rs
@@ -1,6 +1,8 @@
 use itertools::Itertools;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
+use std::iter::FromIterator;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut file = File::open("input.txt")?;
@@ -10,18 +12,34 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let groups = contents.split("\n\n");
 
     let part1: usize = groups
+        .clone()
         .map(|group| {
             let mut group: Vec<u8> = group.as_bytes().into();
             group.sort();
-            group
-                .iter()
-                .filter(|v| **v != b'\n')
-                .map(|v| u32::from(*v))
-                .unique()
-                .count()
+            group.iter().filter(|v| **v != b'\n').unique().count()
         })
         .sum();
     println!("Part 1: {}", part1);
+
+    let part2: usize = groups
+        .map(|group| {
+            let people: Vec<HashSet<u8>> = group
+                .lines()
+                .map(|line| HashSet::from_iter(line.bytes()))
+                .collect();
+
+            let first = people[0].clone();
+            people
+                .iter()
+                .fold(first, |acc, next| {
+                    acc.intersection(&next).cloned().collect()
+                })
+                .iter()
+                .count()
+        })
+        .sum();
+
+    println!("Part 2: {}", part2);
 
     Ok(())
 }
