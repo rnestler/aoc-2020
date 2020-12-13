e252c95ad60ccf1ca0fa0970b14525dbde1fc24a
Raphael Nestler <raphael.nestler@gmail.com>
2020-12-13 15:45:44 +0100

    Add day 13 part 1

--- aoc-2020-13/src/main.rs
+++ aoc-2020-13/src/main.rs
@@ -6,5 +6,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
+    let mut lines = contents.lines();
+    let earliest_start: u32 = lines.next().unwrap().parse().unwrap();
+    let buses: Vec<u32> = lines
+        .next()
+        .unwrap()
+        .split(',')
+        .filter_map(|s| s.parse().ok())
+        .collect();
+
+    let min = buses
+        .iter()
+        .map(|bus| (earliest_start / bus + 1) * bus)
+        .enumerate()
+        .min_by_key(|x| x.1)
+        .unwrap();
+
+    println!("Part 1: {}", buses[min.0] * (min.1 - earliest_start));
+
     Ok(())
 }
